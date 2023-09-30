bdfa86d482e4da463292a1a4885b0ef10c11a4f9
aleksander <aleksbro@stud.ntnu.no>
2023-09-30 17:10:06 +0200

    Error stop calling real api when in dev mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
@@ -7,21 +7,47 @@ import router from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "./theme";
 
-// Mock for development
-if (process.env.NODE_ENV === "development") {
-  import("./mocks").then(() => {
-    console.log("Mocks loaded");
-  });
-}
+const App = () => {
+  const [mocksLoaded, setMocksLoaded] = useState(false);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "development") {
+      import("./mocks").then(() => {
+        console.log("Mocks loaded");
+        setMocksLoaded(true);
+      });
+    } else {
+      setMocksLoaded(true);
+    }
+  }, []);
+
+  if (!mocksLoaded) return <div>Loading...</div>;
+
+  return (
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+};
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </ChakraProvider>
-  </React.StrictMode>
+  <App />
 );
+
+// const queryClient = new QueryClient();
+
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <ChakraProvider theme={theme}>
+//       <QueryClientProvider client={queryClient}>
+//         <RouterProvider router={router} />
+//       </QueryClientProvider>
+//     </ChakraProvider>
+//   </React.StrictMode>
+// );
